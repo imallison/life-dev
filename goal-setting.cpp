a6de1d03882982d1ceb40f751a37bbcaa48fe0d1
Allison <138368973+allison-pham@users.noreply.github.com>
2024-02-14 10:52:44 -0800

    Add main.cpp testing

---
+++ goal-setting.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+class GoalSetting
+{
+public:
+    int goalCounter;
+    std::vector<string> goals;
+    string goalStatus;
+    string proficiency;
+};
--- implementation/general.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "general.h"
-
-// get and set username
-// select accountType from a vector of types
-// get and set focus;
