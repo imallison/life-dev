2c417fa4db7a09153360b7769f91a7b49ad83c16
Allison <138368973+allison-pham@users.noreply.github.com>
2024-02-14 05:37:13 -0800

    Adjust general header

--- header/general.h
+++ header/general.h
@@ -2,20 +2,37 @@
 #include <vector>
 using namespace std;
 
-class General { // Change class to struct
-    public:
-        string username;
-        std::vector<string> accountType; // Choice between study, career, and project
-        string focus; // Example: Math
-    
+class General
+{
+private:
+    std::string username;
+    std::vector<std::string> accountType; // Choice between study, career, and project
+    std::string focus;                    // Example: Math
+public:
     General(); // Constructor
+    General(const std::string &username, const std::vector<std::string> &accountType, const std::string &focus);
+
+    // Mutators
+    void setUsername(const std::string &username);
+    void setAccountType(const std::vector<std::string> &accountType);
+    void setFocus(cons t std::string &focus);
+
+    // Accessors
+    std::string getUsername() const;
+    std::vector<std::string> getAccountType() const;
+    std::string getFocus() const;
+
+    // Other
+    void shareFocus(General &other);
+
     // Classes are private by default, while structs are public by default
     // Constructs must always be public
     // Getters (usually 1 line long) & setters
 };
 
-General::General() { // Default constructor
+/*General::General()
+{ // Default constructor
     username = "";
     accountType = {""};
     focus = "";
-};
+};*/
