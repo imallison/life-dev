482a0b18100fa32d7dddc04d3a6760cb9fc212f5
Allison <138368973+allison-pham@users.noreply.github.com>
2024-03-01 15:53:29 -0800

    Add goal, proficiency, and status functionality

--- development/goal_setting.h
+++ development/goal_setting.h
@@ -7,21 +7,28 @@ using namespace std;
 
 class GoalSetting
 {
+private:
+    std::vector<std::string> goals; // Define
+    std::string proficiency;
+    std::string status;
+
 public:
-    int goalCounter;
-    std::vector<string> goals;
-    string goalStatus;
-    string proficiency;
-
     GoalSetting();
+
+    void addGoal(const std::string &goal);
+    std::vector<std::string> getGoals() const;
+    std::string getGoalStatus() const;
+    std::string getProficiency() const;
+    void setProficiency(const std::string &level);
+    void updateGoalStatus(const std::string &status);
 };
 
-GoalSetting::GoalSetting()
-{
-    goalCounter = 0;
-    goals = {""};
-    goalStatus = "";
-    proficiency = "";
-};
+// GoalSetting::GoalSetting()
+// {
+//     goalCounter = 0;
+//     goals = {""};
+//     goalStatus = "";
+//     proficiency = "";
+// };
 
 #endif
